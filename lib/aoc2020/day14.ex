67c9d9c5fd80e9677a1621bd9e7e2996199a33ac
Joel C <joel@vorce.se>
2020-12-14 21:07:34 +0100

    Implement Day 14, part 2 (#6)
    It ain't pretty or fast, but it works.

--- lib/aoc2020/day14.ex
+++ lib/aoc2020/day14.ex
@@ -1,27 +1,38 @@
 defmodule Aoc2020.Day14 do
   defstruct [:mask, :instructions]
 
-  def parse!(lines) do
+  def parse!(lines, part) do
+    parse_line_fn =
+      case part do
+        :part1 -> &parse_instruction/1
+        :part2 -> &parse_instruction2/1
+      end
+
     Enum.map(lines, fn line ->
       line
       |> String.codepoints()
-      |> parse_instruction()
+      |> parse_line_fn.()
     end)
   end
 
   def parse_instruction(["m", "a", "s", "k", " ", "=", " " | val]) do
-    val =
+    map =
       val
       |> Enum.reverse()
       |> Enum.with_index()
       |> Enum.reject(fn {elem, _index} -> elem == "X" end)
-      |> Enum.into(%{}, fn {elem, index} -> {index, String.to_integer(elem)} end)
+      |> Enum.into(%{}, fn {elem, index} ->
+        if elem == "X" do
+          {index, elem}
+        else
+          {index, String.to_integer(elem)}
+        end
+      end)
 
-    %{op: :mask, val: val}
+    %{op: :mask, val: map}
   end
 
   def parse_instruction(mem) do
-    # mem[8] = 11
     line = Enum.join(mem)
     [op, val] = String.split(line, " = ")
     [op, index] = String.split(op, "[")
@@ -33,13 +44,93 @@ defmodule Aoc2020.Day14 do
     }
   end
 
-  def execute(instructions) do
+  def parse_instruction2(["m", "a", "s", "k", " ", "=", " " | val]) do
+    map =
+      val
+      |> Enum.reverse()
+      |> Enum.with_index()
+      |> Enum.into(%{}, fn {elem, index} ->
+        if elem == "X" do
+          {index, elem}
+        else
+          {index, String.to_integer(elem)}
+        end
+      end)
+
+    %{op: :mask, val: map}
+  end
+
+  def parse_instruction2(mem) do
+    parse_instruction(mem)
+  end
+
+  def execute(instructions, part) do
+    run_fn =
+      case part do
+        :part1 -> &run_instruction/3
+        :part2 -> &run_instruction2/3
+      end
+
     {mem, _mask} =
       Enum.reduce(instructions, {%{}, %{}}, fn instruction, {mem, mask} ->
-        run_instruction(instruction, mem, mask)
+        run_fn.(instruction, mem, mask)
       end)
 
     mem
+  end
+
+  def mask_to_string(mask_map) do
+    Enum.reduce(35..0, "", fn i, acc -> "#{acc}#{Map.get(mask_map, i)}" end)
+  end
+
+  def run_instruction2(%{op: :mask} = instruction, mem, _mask) do
+    IO.puts("Setting mask: #{mask_to_string(instruction.val)}")
+    {mem, instruction.val}
+  end
+
+  def run_instruction2(%{op: :mem} = instruction, mem, mask) do
+    IO.puts("Running instruction: #{instruction.i}")
+
+    result =
+      instruction.i
+      |> binary_map()
+      |> apply_mask2(mask)
+
+    result_size = map_size(result)
+
+    new_mem =
+      result
+      |> build_tree()
+      |> paths([])
+      |> Enum.chunk_by(fn node -> node == nil end)
+      |> Enum.reject(fn x ->
+        length(x) != result_size
+      end)
+      |> Enum.into(%{}, fn {:ok, path} ->
+        key =
+          Enum.with_index(path)
+          |> Enum.into(%{}, fn {elem, index} -> {index, elem} end)
+          |> to_decimal()
+
+        {key, instruction.val}
+      end)
+
+    {Map.merge(mem, new_mem), mask}
+  end
+
+  defp build_tree(mask_on_address_map) do
+    Enum.reduce(mask_on_address_map, tree(), fn {_k, v}, acc ->
+      add(acc, v)
+    end)
+  end
+
+  def apply_mask2(bin_map, mask_map) do
+    Enum.reduce(0..(map_size(mask_map) - 1), %{}, fn i, acc ->
+      case Map.get(mask_map, i) do
+        0 -> Map.put(acc, i, Map.get(bin_map, i, 0))
+        other -> Map.put(acc, i, other)
+      end
+    end)
   end
 
   def run_instruction(%{op: :mem} = instruction, mem, mask) do
@@ -57,25 +148,100 @@ defmodule Aoc2020.Day14 do
   end
 
   def apply_mask(value, mask) do
-    # Integer.digits(11, 2)
-    # [1, 0, 1, 1]
-    binary_val =
-      Integer.digits(value, 2)
-      |> Enum.reverse()
-      |> Enum.with_index()
-      |> Enum.into(%{}, fn {elem, index} -> {index, elem} end)
+    binary_val = binary_map(value)
 
     Map.merge(binary_val, mask)
-    |> Enum.reduce(0, fn {index, val}, acc ->
+    |> to_decimal()
+  end
+
+  def to_decimal(bin_map) do
+    Enum.reduce(bin_map, 0, fn {index, val}, acc ->
       new_val = round(:math.pow(2, index)) * val
       result = acc + new_val
       result
     end)
   end
 
+  def binary_map(decimal) do
+    decimal
+    |> Integer.digits(2)
+    |> Enum.reverse()
+    |> Enum.with_index()
+    |> Enum.into(%{}, fn {elem, index} -> {index, elem} end)
+  end
+
   def sum_memory(mem) do
     mem
     |> Map.values()
     |> Enum.sum()
+  end
+
+  def tree(), do: %{val: nil, left: nil, right: nil}
+  def tree(val), do: %{val: val, left: nil, right: nil}
+
+  def add(%{val: _, left: nil, right: nil} = node, "X") do
+    %{node | left: tree(0), right: tree(1)}
+  end
+
+  def add(%{val: _, left: nil, right: r} = node, "X") do
+    %{node | left: tree(0), right: add(r, "X")}
+  end
+
+  def add(%{val: _, left: l, right: nil} = node, "X") do
+    %{node | left: add(l, "X"), right: tree(1)}
+  end
+
+  def add(%{val: _, left: l, right: r} = node, "X") do
+    %{node | left: add(l, "X"), right: add(r, "X")}
+  end
+
+  def add(%{val: _, left: nil, right: nil} = node, 0) do
+    %{node | left: tree(0)}
+  end
+
+  def add(%{val: _, left: nil, right: r} = node, 0) do
+    %{node | right: add(r, 0)}
+  end
+
+  def add(%{val: _, left: l, right: nil} = node, 0) do
+    %{node | left: add(l, 0)}
+  end
+
+  def add(%{val: _, left: l, right: r} = node, 0) do
+    %{node | left: add(l, 0), right: add(r, 0)}
+  end
+
+  def add(%{val: _, left: nil, right: nil} = node, 1) do
+    %{node | right: tree(1)}
+  end
+
+  def add(%{val: _, left: nil, right: r} = node, 1) do
+    %{node | right: add(r, 1)}
+  end
+
+  def add(%{val: _, left: l, right: nil} = node, 1) do
+    %{node | left: add(l, 1)}
+  end
+
+  def add(%{val: _, left: l, right: r} = node, 1) do
+    %{node | left: add(l, 1), right: add(r, 1)}
+  end
+
+  def paths(%{val: val, left: nil, right: nil}, acc), do: Enum.reverse([val | acc])
+
+  def paths(%{val: val, left: nil, right: r}, acc) do
+    paths(r, [val | acc])
+  end
+
+  def paths(%{val: val, left: l, right: nil}, acc) do
+    paths(l, [val | acc])
+  end
+
+  def paths(%{val: val, left: l, right: r}, acc) do
+    [
+      paths(l, [val | acc]),
+      paths(r, [val | acc])
+    ]
+    |> List.flatten()
   end
 end
--- test/day14_test.exs
+++ test/day14_test.exs
@@ -12,7 +12,7 @@ defmodule Aoc2020.Day14Test do
 
   describe "part 1" do
     test "parse!/1" do
-      assert Day14.parse!(@example) == [
+      assert Day14.parse!(@example, :part1) == [
                %{op: :mask, val: %{1 => 0, 6 => 1}},
                %{op: :mem, i: 8, val: 11},
                %{op: :mem, i: 7, val: 101},
@@ -21,18 +21,18 @@ defmodule Aoc2020.Day14Test do
     end
 
     test "execute/1" do
-      prog = Day14.parse!(@example)
+      prog = Day14.parse!(@example, :part1)
 
-      assert Day14.execute(prog) == %{
+      assert Day14.execute(prog, :part1) == %{
                7 => 101,
                8 => 64
              }
     end
 
     test "sum_memory/1" do
-      prog = Day14.parse!(@example)
+      prog = Day14.parse!(@example, :part1)
 
-      mem = Day14.execute(prog)
+      mem = Day14.execute(prog, :part1)
 
       assert Day14.sum_memory(mem) == 165
     end
@@ -43,14 +43,131 @@ defmodule Aoc2020.Day14Test do
         |> File.read!()
         |> String.split("\n")
         |> Enum.reject(fn line -> line == "" end)
-        |> Day14.parse!()
+        |> Day14.parse!(:part1)
 
-      mem = Day14.execute(prog)
+      mem = Day14.execute(prog, :part1)
 
       assert Day14.sum_memory(mem) == 6_317_049_172_545
     end
   end
 
   describe "part 2" do
+    test "parse mask" do
+      mask = "mask = 001X11X1X010X1X1010XX10X100101011000"
+
+      assert Day14.parse_instruction2(String.codepoints(mask)) == %{
+               op: :mask,
+               val: %{
+                 11 => 1,
+                 34 => 0,
+                 26 => 0,
+                 15 => "X",
+                 20 => 1,
+                 17 => 0,
+                 25 => 1,
+                 13 => 0,
+                 0 => 0,
+                 8 => 1,
+                 7 => 0,
+                 1 => 0,
+                 32 => "X",
+                 35 => 0,
+                 3 => 1,
+                 6 => 1,
+                 2 => 0,
+                 33 => 1,
+                 10 => 0,
+                 9 => 0,
+                 19 => 0,
+                 14 => 1,
+                 5 => 0,
+                 18 => 1,
+                 31 => 1,
+                 22 => 1,
+                 29 => "X",
+                 21 => "X",
+                 27 => "X",
+                 24 => 0,
+                 30 => 1,
+                 23 => "X",
+                 28 => 1,
+                 16 => "X",
+                 4 => 1,
+                 12 => "X"
+               }
+             }
+    end
+
+    test "execute example" do
+      input = [
+        "mask = 000000000000000000000000000000X1001X",
+        "mem[42] = 100",
+        "mask = 00000000000000000000000000000000X0XX",
+        "mem[26] = 1"
+      ]
+
+      prog = Day14.parse!(input, :part2)
+      mem = Day14.execute(prog, :part2)
+      assert Day14.sum_memory(mem) == 208
+    end
+
+    @tag timeout: :infinity
+    test "execute2 on input file" do
+      prog =
+        "test/data/day14_input.txt"
+        |> File.read!()
+        |> String.split("\n")
+        |> Enum.reject(fn line -> line == "" end)
+        |> Day14.parse!(:part2)
+
+      mem = Day14.execute(prog, :part2)
+
+      # 3404936041419: That's not the right answer; your answer is too low.
+      assert Day14.sum_memory(mem) == 3_434_009_980_379
+    end
+
+    @tag timeout: :infinity
+    test "execute2 on part of input file" do
+      prog =
+        [
+          "mask = 001X11X1X010X1X1010XX10X100101011000",
+          "mem[43398] = 563312",
+          "mem[51673] = 263978",
+          "mem[18028] = 544304215",
+          "mask = X0100001101XX11100010XX110XX11111000",
+          "mem[24151] = 2013",
+          "mem[15368] = 19793",
+          "mem[45005] = 478",
+          "mem[1842] = 190808161",
+          "mem[36033] = 987",
+          "mem[26874] = 102",
+          "mask = 00X0000110110X000110010101XX0X010001",
+          "mem[9507] = 7",
+          "mem[50019] = 16475608",
+          "mem[4334] = 129799",
+          "mem[37373] = 182640",
+          "mem[28170] = 534617265",
+          "mem[6432] = 354252",
+          "mem[36752] = 834628",
+          "mask = 10100000101101100110X001X0X001100X10",
+          "mem[36664] = 30481",
+          "mem[6532] = 103013119",
+          "mem[45659] = 15629",
+          "mem[19533] = 167227",
+          "mem[40461] = 344193233",
+          "mem[6217] = 26713310",
+          "mask = X0XX010100110101X0001101X11100X100X0",
+          "mem[38530] = 6202",
+          "mem[53032] = 13775",
+          "mem[39333] = 1003152",
+          "mem[3932] = 1240562",
+          "mem[59246] = 12638"
+        ]
+        |> Day14.parse!(:part2)
+
+      mem = Day14.execute(prog, :part2)
+
+      assert Day14.sum_memory(mem) == 3_434_009_980_379
+    end
   end
 end
