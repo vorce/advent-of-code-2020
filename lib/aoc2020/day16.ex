d691a832fad4dc02b6443afd125befe6efef0600
Joel C <joel@vorce.se>
2020-12-17 21:39:09 +0100

    Day 16, part 2 attempts

--- lib/aoc2020/day16.ex
+++ lib/aoc2020/day16.ex
@@ -46,4 +46,66 @@ defmodule Aoc2020.Day16 do
     end)
     |> Enum.sum()
   end
+
+  def determine() do
+    """
+    class: 0-1 or 4-19
+    row: 0-5 or 8-19
+    seat: 0-13 or 16-19
+
+    your ticket:
+    11,12,13
+
+    nearby tickets:
+    3,9,18
+    15,1,5
+    5,14,9
+    """
+
+    # rule[0] = [0..1, 4..19]
+    # rule[1] = [0..5, 8..19]
+    # ...
+    # rule[n] = [r1i..r2i, r1j..r2j]
+    #
+    # col[0] = rule[k] such that each ticket[i][0] = within rule[k][0] or rule[k][1]
+    # can we rule out a column?
+    # find_rule_col():
+    # for each ticket t:
+    #   [t-valid-for-rule[0]]
+  end
+
+  def ticket_column([r1, r2], tickets, taken) do
+    Enum.map(tickets, fn ticket ->
+      ticket
+      |> Enum.with_index()
+      |> Enum.map(fn {nr, i} ->
+        {nr in r1 or nr in r2, i}
+      end)
+    end)
+    |> Enum.reject(fn matches -> Enum.any?(matches, fn {m, _i} -> m == false end) end)
+    |> Enum.flat_map(fn matches -> Enum.map(matches, fn {_m, i} -> i end) end)
+  end
+
+  def distribute_columns(candidates) do
+    Enum.sort(candidates, fn {_, c1}, {_, c2} ->
+      length(c1) < length(c2)
+    end)
+    |> Enum.reduce(%{}, fn {col, cs}, acc ->
+      c = first_available(acc, cs)
+      # IO.inspect([col: col, candidates: cs, first_available: c, acc: acc], label: "map to cols")
+      Map.put(acc, c, col)
+    end)
+  end
+
+  def first_available(acc, cs) do
+    Enum.find(cs, fn c -> not Map.has_key?(acc, c) end)
+  end
+
+  def candidates(rules, nearby) do
+    rules
+    |> Enum.with_index()
+    |> Enum.map(fn {rule, i} ->
+      {i, ticket_column(rule, nearby, [])}
+    end)
+  end
 end
--- test/day16_text.exs
+++ test/day16_text.exs
@@ -44,5 +44,111 @@ defmodule Aoc2020.Day16Test do
   end
 
   describe "part 2" do
+    test "ticket_column" do
+      rule0 = [0..1, 4..19]
+      rule1 = [0..5, 8..19]
+      rule2 = [0..13, 16..19]
+
+      nearby = [
+        [3, 9, 18],
+        [15, 1, 5],
+        [5, 14, 9]
+      ]
+
+      assert Day16.ticket_column(rule0, nearby, []) == [1, 2]
+      assert Day16.ticket_column(rule1, nearby, []) == [0, 1, 2]
+      assert Day16.ticket_column(rule2, nearby, []) == [0]
+
+      # [[1, 2], [0, 1, 2], [0]]
+    end
+
+    test "distribute_columns" do
+      candidates = [{0, [1, 2]}, {1, [0, 1, 2]}, {2, [0]}]
+      assert Day16.distribute_columns(candidates) == %{0 => 2, 1 => 0, 2 => 1}
+      # keys=column, value=rule
+      # [{2, 0}, {0, 1}, {1, 2}]
+    end
+
+    test "candidates on example" do
+      rules = [[0..1, 4..19], [0..5, 8..19], [0..13, 16..19]]
+
+      nearby = [
+        [3, 9, 18],
+        [15, 1, 5],
+        [5, 14, 9]
+      ]
+
+      assert Day16.candidates(rules, nearby) == [{0, [1, 2]}, {1, [0, 1, 2]}, {2, [0]}]
+    end
+
+    test "map cols on input" do
+      lines =
+        "test/data/day16_input.txt"
+        |> File.read!()
+        |> String.split("\n")
+        |> Enum.reject(fn line -> line == "" end)
+
+      rule_lines = Enum.take(lines, 20)
+
+      rules = Day16.parse_rules(rule_lines)
+
+      nearby_lines = Enum.drop(lines, 23)
+
+      nearby_tickets = Day16.parse_nearby(nearby_lines)
+
+      # keys=column, value=rule
+      mapping =
+        rules
+        |> Day16.candidates(nearby_tickets)
+        |> IO.inspect(label: "candidates")
+        |> Day16.distribute_columns()
+        |> IO.inspect(label: "distributed")
+
+      # keys=rule, value=column
+      reversed_mapping = Enum.into(mapping, %{}, fn {column, rule} -> {rule, column} end)
+
+      cols =
+        [
+          Map.get(reversed_mapping, 0),
+          Map.get(reversed_mapping, 1),
+          Map.get(reversed_mapping, 2),
+          Map.get(reversed_mapping, 3),
+          Map.get(reversed_mapping, 4),
+          Map.get(reversed_mapping, 5)
+        ]
+        |> IO.inspect(label: "columns for departure*")
+
+      my_ticket =
+        [
+          113,
+          197,
+          59,
+          167,
+          151,
+          107,
+          79,
+          73,
+          109,
+          157,
+          199,
+          193,
+          83,
+          53,
+          89,
+          71,
+          149,
+          61,
+          67,
+          163
+        ]
+        |> Enum.with_index()
+        |> Enum.map(fn {elem, i} -> {i, elem} end)
+        |> Enum.into(%{})
+
+      # [20, 16, 22, 14, 19, 13]
+      assert Enum.reduce(cols, 1, fn col, acc ->
+               acc * Map.get(my_ticket, col)
+             end) == 0
+    end
   end
 end
